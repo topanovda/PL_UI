959ad2c4203620aef6ba61d45071e7f9c9d49237
topanovda <103432340+topanovda@users.noreply.github.com>
2025-11-01 16:00:18 +0300

    Update test_login_page.py
    сломал тест для проверки прикрепления скриншота в allure

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -9,6 +9,6 @@ class TestLoginPage(BaseTest):
     @pytest.mark.login
     def test_login(self):
         self.login_page.open()
-        self.login_page.login_as("admin")
+        self.login_page.login_as("admi n")
         with allure.step("Проверка что страница открыта"):
             self.project_page.is_page_opened()
